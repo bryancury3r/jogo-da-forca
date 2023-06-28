302abb84a2edb1d062df64c2bce32349abedb828
Gabriel Cury <gabriel_bryancury@outlook.com>
2023-06-27 21:07:56 -0300

    add styled-components + base and vertical

--- src/components/hangman-drawing.tsx
+++ src/components/hangman-drawing.tsx
@@ -1,7 +1,36 @@
+import styled from "styled-components"
+
+const Base = styled.div `
+    height: 10px;
+    width: 250px;
+    background: black;
+
+`
+
+const Vertical = styled.div `
+    height: 250px;
+    width: 10px;
+    background: black;
+
+`
+
+
+
+
+
+
 export default function HangmanDrawing() {
     return (
-            <div>
-                hangman-drawing
+            <div style={{ 
+                position: 'relative', 
+                display: 'flex', 
+                flexDirection: 'column', 
+                alignItems: 'center'
+            }}
+                >
+                <div>1</div>
+                <Vertical />
+                <Base />
             </div>
         )
 }
